4a00bae6fd32e3f6e995f73f9b382822b6e2bc6b
luanesouza <luanesouza411@gmail.com>
2019-12-13 11:57:12 -0500

    adds styled components package

---
+++ reconnect_client_side_2_0/src/components/user/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../Logo';
+import { Link } from 'react-router-dom';
+
+function UserHeader(props){
+  return(
+    <nav>
+      <h2>This is a navbar</h2>
+
+    </nav>
+  )
+}
+
+export default UserHeader;
