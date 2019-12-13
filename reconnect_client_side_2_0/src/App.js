f4eb595fc0fb92b82afb3ff458eccf32144a43a1
luanesouza <luanesouza411@gmail.com>
2019-12-13 11:55:05 -0500

    restructuring app

---
+++ reconnect_client_side_2_0/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+    </main>
+  );
+}
+
+export default App;
