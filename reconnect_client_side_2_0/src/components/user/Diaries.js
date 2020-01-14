68de4060cb593eea5c56d6602ff32f529c904749
luanesouza <luanesouza411@gmail.com>
2020-01-14 15:02:10 -0500

    changes style to make code more readable

--- reconnect_client_side_2_0/src/components/user/Diaries.js
+++ reconnect_client_side_2_0/src/components/user/Diaries.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import { DiarySection } from './styles'
 
-import { allDiaries, oneDiary } from './api-data'
-import Diary from './Diary'
-
 export default function Diaries(props){
 
+    const diaries = props.diaries.map( (diary, idx) => (
+      <DiarySection
+        key={idx}
+        image_url={diary.image_url}
+        onClick={() => props.diaryShowPage(diary.id)}
+        >
 
-      const diaries = props.diaries.map( (diary, idx) => (
-        <DiarySection key={idx} image_url={diary.image_url}  onClick={() => props.diaryShowPage(diary.id)}>
-          <h1>{diary.title} </h1>
-        </DiarySection >
-        )
+        <h1> {diary.title} </h1>
+        
+      </DiarySection >
       )
+    )
+
     return (
       <section>
         {
