b8d0d2d71f3c7807743029a3fbf291fb61f56bc8
luanesouza <luanesouza411@gmail.com>
2020-01-17 18:04:00 -0500

    adds form handle change

--- reconnect_client_side_2_0/src/components/user/UserLoginForm.js
+++ reconnect_client_side_2_0/src/components/user/UserLoginForm.js
@@ -3,15 +3,12 @@ import { LoginFormSection, SignupFormSection } from './styles'
 
 export default class UserLoginForm extends React.Component {
   state = {
-    loginObj: {
-      email: '',
-      password: ''
-    },
-    signupObj: {
-      email: '',
-      password: '',
-      name: ''
-    }
+    signupName: '',
+    signupEmail: '',
+    signupPassword: '',
+    email: '',
+    password: '',
+
   }
 
   render() {
@@ -20,19 +17,18 @@ export default class UserLoginForm extends React.Component {
       console.log(evt);
       const {name, value} = evt.target
       this.setState ({
-        loginObj: {
-          [name]:value
-        }
+        [name]:value
       })
     }
 
     const handleLoginSubmit = (e) => {
       e.preventDefault()
-      console.log(this.state.loginObj);
+      console.log(this.state);
     }
 
     const handleSignupSubmit = (e) => {
       e.preventDefault()
+      console.log(this.state.signupObj);
     }
 
 
@@ -45,8 +41,8 @@ export default class UserLoginForm extends React.Component {
       <div class='user-forms'>
         <LoginFormSection>
           <form onSubmit={this.loginOrSignup}>
-            <input onChange={(e) => handleChange(e)} name='email' type='text' value={this.state.email} placeholder='email' autoComplete="off"/>
-            <input onChange={(e) => handleChange(e)} name='password' type='text' value={this.state.password} placeholder='password' autoComplete="off"/>
+            <input onChange={(e) => handleChange(e)} name='email' type='text' value={this.state.loginObj} placeholder='email' autoComplete="off"/>
+            <input onChange={(e) => handleChange(e)} name='password' type='text' value={this.state.loginObj} placeholder='password' autoComplete="off"/>
             <button id='login-button' onClick={(e) => handleLoginSubmit(e)}>Login</button>
           </form>
         </LoginFormSection>
@@ -54,9 +50,9 @@ export default class UserLoginForm extends React.Component {
         <SignupFormSection>
           <img src='https://www.graphicsprings.com/filestorage/stencils/3c0d59eba085b4c25dba55b6f5afdf6d.png?width=500&height=500'/>
           <form>
-            <input type='text' value={this.state.name} placeholder='name' autoComplete="off"/>
-            <input type='text' value={this.state.email} placeholder='email' autoComplete="off"/>
-            <input type='text' value={this.state.password} placeholder='password' autoComplete="off"/>
+            <input type='text' value={this.state.signupName} placeholder='signupName' autoComplete="off"/>
+            <input type='text' value={this.state.signupEmail} placeholder='signupEmail' autoComplete="off"/>
+            <input type='text' value={this.state.signupPassword} placeholder='signupPassword' autoComplete="off"/>
             <button onClick={(e) => handleSignupSubmit(e)}>Join Us</button>
           </form>
         </SignupFormSection>
