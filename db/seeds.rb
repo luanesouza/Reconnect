cea1e3f7248a9b882672530672b270a6ba2e8c5b
luanesouza <luanesouza411@gmail.com>
2020-04-14 10:13:12 -0400

    updates gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Therapist.destroy_all()
+
 therapist = Therapist.create([
     {therapist_first_name: 'Penguin',
     therapist_last_name: 'McCarrol',
     bio: 'I opened my first practice in the Medical Arts Building at Northwest Hospital.  I practiced there over a year before moving to my current office at 7201 5th Avenue NE.
     While at Northwest, I was invited by Bill Turska N.D. in Mist, Oregon to come and help him.  He was my first mentor.  He was an old-timey Naturopath that had a history of incredible cures.  I would work with him on Mondays, Wednesdays, and Saturdays, and at my practice in Seattle on Tuesdays, Thursdays, and Fridays.  One night when we had finished working with clients, Dr. Turska and I stayed up and talked for a long time into the night.  When I woke up the next morning, I felt different, like I was no longer a young Doc working with a mentor but two colleagues working together.  It wasn\'t his words to me, it was something else.  He would talk about how he worked on patients through the ethers, and I think he worked on me.',
     therapist_email: 'penguin@gmail.com',
-    therapist_image: 'https://static1.squarespace.com/static/530c423be4b008955c6eb3fa/59edf36f9f07f53bd97c0ccf/5a5cce5fc83025289aac4550/1516031617093/ZSMITH-2017-04-30-4366+HiRes.jpg',
+    therapist_image: 'https://as1.ftcdn.net/jpg/02/85/47/84/500_F_285478435_lYaLSqd9C8ApI0Fl1XROLNQ4PqUHUij6.jpg',
     therapist_password_digest: 'idkidkidk',
     location: 'Greater New York',
     therapist_skype: 'pengguin',
@@ -24,10 +26,10 @@ therapist = Therapist.create([
      therapist_last_name: 'Brown',
      bio: '',
      therapist_email: 'mb@gmail.com',
-     therapist_image: 'https://www.taraflannery.com/wp-content/uploads/12-6789-page/20180601_Alfred_002(pp_w600_h900).jpg',
+     therapist_image: 'https://as2.ftcdn.net/jpg/02/85/48/23/500_F_285482312_F1lMWYFYDyvbYw8gsQCJrs8gBkrMnd8s.jpg',
      therapist_password_digest: 'omgomgomgomgo',
      location: 'Greater New York',
-     therapist_skype: 'sweetboy',
+     therapist_skype: 'mb_doctor',
      specialty: 'Go and be the Doctor that I could never be. Then, all things considered...it\'s time I grew up. Well, who are you boys? Oh, of course! Are you his companions? Are you capable of speaking without flapping your hands about? I don\'t have a future. Doctor no more. Do you have to talk like children? What is it that makes you so ashamed of being a grown-up? Great men are forged in fire. It is the privilege of lesser men to light the flame. Whatever the cost. Oh, the pointing again! They\'re screwdrivers! What are you going to do? Assemble a cabinet at them?',
      education: 'History sometimes gives us a terrible shock, and that is because we don\'t quite fully understand. Why should we? After all, we\'re too small to realise its final pattern. Therefore don\'t try and judge it from where you stand. It all started out as a mild curiosity in the junkyard, and now it\'s turned out to be quite a great spirit of adventure. You don\'t understand so you find excuses. As we learn about each other, so we learn about ourselves. The least important things, sometimes my dear boy, lead to the greatest discoveries.',
      price: '200 dollars'
@@ -36,10 +38,10 @@ therapist = Therapist.create([
       therapist_last_name: 'Gillen',
       bio: 'One day while I was walking across campus, a friend called to me.  She handed me a paper with the name "John Bastyr College of Naturopathic Medicine" written on it.  I felt a flash like lightening and shouted, "That\'s it, I\'m going to be a Naturopathic doctor!  I\'m grateful to her everyday for that note. I finished my Biology/ Pre-med, interviewed at John Bastyr College of Naturopathic Medicine and was accepted.  Also, I joined the U.S. Army as a medic which helped pay the way to fulfill my dream. I went to school with a great group of people at John Bastyr College of Naturopathic Medicine.  We worked hard, studied hard, and supported each other to get through the program and graduate. I\'m proud of my class, as they have truly contributed to the advancement of Naturopathic medicine, and still do. The year we graduated the college name changed to Bastyr University.',
       therapist_email: 'dg@gmail.com',
-      therapist_image: 'https://i1.wp.com/www.headshotsla.com/wp-content/uploads/2017/06/Headshots-Los-Angeles_181A8217.jpg?fit=600%2C900&ssl=1',
+      therapist_image: 'https://as1.ftcdn.net/jpg/02/85/47/70/500_F_285477058_47VCitO3BhQmRJkxFth82mSeaE8TNNpH.jpg',
       therapist_password_digest: 'omgomaaaaago',
       location: 'Greater New York',
-      therapist_skype: 'gillenboy',
+      therapist_skype: 'dr_gillen',
       specialty: 'Go and be the Doctor that I could never be. Then, all things considered...it\'s time I grew up. Well, who are you boys? Oh, of course! Are you his companions? Are you capable of speaking without flapping your hands about? I don\'t have a future. Doctor no more. Do you have to talk like children? What is it that makes you so ashamed of being a grown-up? Great men are forged in fire. It is the privilege of lesser men to light the flame. Whatever the cost. Oh, the pointing again! They\'re screwdrivers! What are you going to do? Assemble a cabinet at them?',
       education: 'History sometimes gives us a terrible shock, and that is because we don\'t quite fully understand. Why should we? After all, we\'re too small to realise its final pattern. Therefore don\'t try and judge it from where you stand. It all started out as a mild curiosity in the junkyard, and now it\'s turned out to be quite a great spirit of adventure. You don\'t understand so you find excuses. As we learn about each other, so we learn about ourselves. The least important things, sometimes my dear boy, lead to the greatest discoveries.',
       price: '200 dollars'
@@ -49,7 +51,7 @@ therapist = Therapist.create([
        bio: 'ProfesHor of Pediatrics at the University of Michigan Medical School and Professor at the Medical School and at the University of Michigan School of Public Health. Dr Smith attended Brown University for her undergraduate education anhistory sometimes gives us a terrible shock, and that is because we don\'t quite fully understand. Why should we? After all, we\'re too small to realise its final pattern. Therefore don\'t try and judge it from where you stand. It all started out as a mild curiosity in the junkyard, and now it\'s turned out to be quite a great spirit of adventure. You don\'t understand so you find excuses. As we learn about each other, so we learn about ourselves. The least important things, sometimes my dear boy, lead to the greatest discoveries.d the University of Pennsylvania for her medical degree, where she was elected to the Alpha Omega Alpha Honors Society. She completed internship and
        residency in General Pediatrics at the Boston Combined Residency Program (Childrens Hospital, Boston andBoston Medical Center). She completed dual training in Pediatric Endocrinology and Pediatric Health Services Research and received a Master in Public Health from the Department of Health Management and Policy at the University of Michigan.',
        therapist_email: 'ms@gmail.com',
-       therapist_image: 'https://s-media-cache-ak0.pinimg.com/originals/27/15/71/2715714bf99df2e85f424b6f9fe3778b.jpg',
+       therapist_image: 'https://as2.ftcdn.net/jpg/02/85/48/23/500_F_285482351_ou5gWEIoBEMEQ8uGFnwO0mV1X2YvzHRe.jpg',
        therapist_password_digest: 'seriously',
        location: 'Brooklyn',
        therapist_skype: 'drsmith',
@@ -61,7 +63,7 @@ therapist = Therapist.create([
         therapist_last_name: 'Dellitalia',
         bio: 'Dr. Dellitalia earned his Bachelor of Arts degree in Neurobiology (1984) and his Medical Degree (1989) at the University of Virginia. Following residency in Emergency Medicine at the University of Pittsburgh, he completed additional training in Pediatrics, Pediatric Emergency Medicine and Pediatric Critical Care at The Children\'s Hospital, Boston. While at Harvard, Dr. Dellitalia`s research training focused upon novel means for the mechanical support of lung injury and for restoring lost control of regional pulmonary blood flow. Upon returning to the University of Virginia as PCCM Faculty, Dr. Dellitalia began to explore erythrocyte-based control of regional blood flow and made the observation that this signaling is disrupted in severe inflammatory states. This observation suggested the hypothesis that acquired injury to erythrocytes alters vascular control, regional blood flow regulation and O2 delivery homeostasis; the Dellitalia Lab pursues this question.',
         therapist_email: 'mattd@gmail.com',
-        therapist_image: 'http://estherescalante.com/wp-content/uploads/2013/11/HeadShoots05-600x900.jpg',
+        therapist_image: 'https://as1.ftcdn.net/jpg/02/85/47/84/500_F_285478435_lYaLSqd9C8ApI0Fl1XROLNQ4PqUHUij6.jpg',
         therapist_password_digest: 'seriouslythough',
         location: 'Queens',
         therapist_skype: 'mattd',
@@ -73,7 +75,7 @@ therapist = Therapist.create([
          therapist_last_name: 'Leung',
          bio: 'He came to Washington University in 2006 as Director of Pediatric Critical Care Medicine. After leading PCCM here for 10 years, Dr. Leung stepped down as Division Director to focus upon his expanding research program, which explores the physiology of acquired red cell injury, translational transfusion medicine in the ICU setting, and development of a novel bio-synthetic artificial red cell to serve as a blood substitute. His global health projects are based in Blantyre, Malawi in collaboration with a large international malaria research group and range from mechanistic studies of infected red cell pathophysiology to clinical pathway implementation for the care of acutely ill children in resource-limited settings.',
          therapist_email: 'eleung@gmail.com',
-         therapist_image: 'https://format-com-cld-res.cloudinary.com/image/private/s--xD-lrdOO--/c_limit,g_center,h_900,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/9fc661083f00a7a4d8b0dba83a349554/Headshots-21.jpg',
+         therapist_image: 'https://as2.ftcdn.net/jpg/02/85/48/23/500_F_285482318_ZKHUUIcPj6xBjt3JZIKHJfRxUWlKyflN.jpg',
          therapist_password_digest: 'seriously',
          location: 'Brooklyn',
          therapist_skype: 'ericc',
@@ -85,7 +87,7 @@ therapist = Therapist.create([
           therapist_last_name: 'Souza',
           bio: 'In 2008 she opened the award-winning Santa Cruz Integrative Medicine Clinic. Dr. Abrams treats many of the world’s most influential people, from CEOs to billionaire entrepreneurs to Nobel Peace laureates. She has been voted “Best Dellitalia” in Santa Cruz County every year, from 2009–2017.',
           therapist_email: 'msouza@gmail.com',
-          therapist_image: 'http://paulamodio.com/wp-content/uploads/2013/11/headshot_50.jpg',
+          therapist_image: 'https://as1.ftcdn.net/jpg/02/85/47/82/500_F_285478280_hkLtznSTC2reb8a2LHW9AeiwVYMZjC4G.jpg',
           therapist_password_digest: 'seriouslyyeahbro',
           location: 'Brooklyn',
           therapist_skype: 'mrsouza',
@@ -99,10 +101,22 @@ therapist = Therapist.create([
        therapist_last_name: 'Cavarron',
        bio: 'In 2009, Dr. Cavarron was named a Brehm Investigator by the Brehm Center for Type 1 Diabetes Research. In 2012, she was a visiting scholar at in Child Health Policy at Stanford University and in 2015 was a visiting scholar at Google Life Sciences (now Verily). At the 2016 Pediatric Academic Societies meeting, she was the first recipient of the Paul Kaplowitz, MD, Endowed Lectureship for contributions to quality and cost-effective care in Pediatric Endocrinology, an award sponsored by the American Academy of Pediatrics and the Pediatric Endocrine Society. Finally, she was named the Robert P. Kelch, MD Research Professor of Pediatrics in 2016. ',
        therapist_email: 'el@gmail.com',
-       therapist_image: 'http://blog.ninaparkerphotography.com/wp-content/uploads/2015/08/Monica-AtlantaExecutive-Headshot-NinaParkerStudios.png',
+       therapist_image: 'https://as1.ftcdn.net/jpg/02/85/47/84/500_F_285478436_y8Bcp3YecDCaTgQxvYn5U8obSoLhNodC.jpg',
        therapist_password_digest: 'idkidaaakidk',
        location: 'Sao Paulo, BR',
        therapist_skype: 'elaine',
+       specialty: 'Go and be the Doctor that I could never be. Then, all things considered...it\'s time I grew up. Well, who are you boys? Oh, of course! Are you his companions? Are you capable of speaking without flapping your hands about? I don\'t have a future. Doctor no more. Do you have to talk like children? What is it that makes you so ashamed of being a grown-up? Great men are forged in fire. It is the privilege of lesser men to light the flame. Whatever the cost. Oh, the pointing again! They\'re screwdrivers! What are you going to do? Assemble a cabinet at them?',
+       education: 'History sometimes gives us a terrible shock, and that is because we don\'t quite fully understand. Why should we? After all, we\'re too small to realise its final pattern. Therefore don\'t try and judge it from where you stand. It all started out as a mild curiosity in the junkyard, and now it\'s turned out to be quite a great spirit of adventure. You don\'t understand so you find excuses. As we learn about each other, so we learn about ourselves. The least important things, sometimes my dear boy, lead to the greatest discoveries.',
+       price: '150 dollars'
+        },
+       {therapist_first_name: 'Ishmet',
+       therapist_last_name: 'Singh',
+       bio: 'In 2009, Dr. Cavarron was named a Brehm Investigator by the Brehm Center for Type 1 Diabetes Research. In 2012, she was a visiting scholar at in Child Health Policy at Stanford University and in 2015 was a visiting scholar at Google Life Sciences (now Verily). At the 2016 Pediatric Academic Societies meeting, she was the first recipient of the Paul Kaplowitz, MD, Endowed Lectureship for contributions to quality and cost-effective care in Pediatric Endocrinology, an award sponsored by the American Academy of Pediatrics and the Pediatric Endocrine Society. Finally, she was named the Robert P. Kelch, MD Research Professor of Pediatrics in 2016. ',
+       therapist_email: 'is@gmail.com',
+       therapist_image: 'https://as2.ftcdn.net/jpg/02/85/48/33/500_F_285483373_SFvWkkyXvib9uKSysjFSmtC7GC878cjR.jpg',
+       therapist_password_digest: 'idkidaaakidk',
+       location: 'Sao Paulo, BR',
+       therapist_skype: 'ish_met',
        specialty: 'Go and be the Doctor that I could never be. Then, all things considered...it\'s time I grew up. Well, who are you boys? Oh, of course! Are you his companions? Are you capable of speaking without flapping your hands about? I don\'t have a future. Doctor no more. Do you have to talk like children? What is it that makes you so ashamed of being a grown-up? Great men are forged in fire. It is the privilege of lesser men to light the flame. Whatever the cost. Oh, the pointing again! They\'re screwdrivers! What are you going to do? Assemble a cabinet at them?',
        education: 'History sometimes gives us a terrible shock, and that is because we don\'t quite fully understand. Why should we? After all, we\'re too small to realise its final pattern. Therefore don\'t try and judge it from where you stand. It all started out as a mild curiosity in the junkyard, and now it\'s turned out to be quite a great spirit of adventure. You don\'t understand so you find excuses. As we learn about each other, so we learn about ourselves. The least important things, sometimes my dear boy, lead to the greatest discoveries.',
        price: '150 dollars'
@@ -115,12 +129,12 @@ users = User.create([
    user_email: 'shiring@gmail.com',
    user_password_digest: 'aaaaa'
    },
-  {user_first_name: 'david',
+  {user_first_name: 'anna',
    user_last_name: 'awesome',
    user_email: 'd@gmail.com',
   user_password_digest: 'aaaaaa'
    },
-   {user_first_name: 'James',
+   {user_first_name: 'aniece',
     user_last_name: 'Bois',
     user_email: 'tupbois@gmail.com',
    user_password_digest: 'aaaaboisboisaa'
@@ -130,12 +144,12 @@ users = User.create([
    user_email: 'drakey@gmail.com',
    user_password_digest: 'aaaaaa'
    },
-  {user_first_name: 'darwin',
+  {user_first_name: 'anna',
    user_last_name: 'smith',
    user_email: 'darwin@gmail.com',
    user_password_digest: 'aaaaaa'}])
 
-# consult = Appointment.create({date: '09/02/2019', time: '01:30 PM', user_id: 4, therapist_id: 2})
+consult = Appointment.create({date: '09/02/2019', time: '01:30 PM', user_id: 1, therapist_id: 1})
 # consult1 = Appointment.create({date: '06/02/2019', time: '01:30 PM', user_id: 3, therapist_id: 1})
 # consult2 = Appointment.create({date: '05/02/2019', time: '01:30 PM', user_id: 2, therapist_id: 2})
 # consult3 = Appointment.create({therapist_first_name: '', therapist_last_name: '', therapist_image: 'http://www.carmelstudiosphotography.com/wp-content/gallery/professional-headshots/Professional-Headshot-6.jpg', date: '04/27/2019', time: '01:30 PM', user_id: 1, therapist_id: 1})
